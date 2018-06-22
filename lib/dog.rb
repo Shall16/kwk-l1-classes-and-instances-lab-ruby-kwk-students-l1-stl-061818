0cbdcb0282161cba38874bfb7d6817134a08230a
Shall16 <shall130@rsdmo.org>
2018-06-22 18:05:44 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,8 @@
-# dog.rb
+class Dog 
+  def initialize( dog_1, dog_2, dog_3) 
+    @dog_1 = dog_1 
+      @dog_2 = dog_2
+      @dog_3 = dog_3
+end
+end 
+
--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,7 @@
-# person.rb
+class Person 
+  def initialize(gender,age) 
+    @gender = gender
+    @age = age
+  end 
+ adele_Goldberg = Person.new("female","72")
+ alan_kay = Person.new("male",)
